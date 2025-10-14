442f3c7309ec107b59367a73ba8236a6c5752c90
Jacob Eaton <jacobr.eaton@calbaptist.edu>
2025-10-14 15:48:05 -0700

    fixing responsiveness for numpicker section

--- src/components/NumPicker.jsx
+++ src/components/NumPicker.jsx
@@ -11,8 +11,8 @@ function NumPicker(props){
 
     return(
         <div className="numPicker">
-            <h2>Pick a Number</h2>
-            <p>Number represents how many hours prior the baloon and weather data will be from the current time.</p>
+            <h2>Select Number of Hours Before Now</h2>
+            <p className="dissappear-medium">Number represents how many hours prior the baloon and weather data will be from the current time.</p>
             <p className="dissappear-medium">(For example, 2 would mean balloon and weather data would be from 2 hours ago)</p>
             <input type="number" onChange={updateVal} value={val} max={23} min={0}/>
         </div>
