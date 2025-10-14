f0a57e3f0f1a9fe23fe264d87b8572df370e080c
Jacob Eaton <jacobr.eaton@calbaptist.edu>
2025-10-14 14:38:24 -0700

    testing more responsive CSS

--- src/components/NumPicker.jsx
+++ src/components/NumPicker.jsx
@@ -13,7 +13,7 @@ function NumPicker(props){
         <div className="numPicker">
             <h2>Pick a Number</h2>
             <p>Number represents how many hours prior the baloon and weather data will be from the current time.</p>
-            <p>(For example, 2 would mean balloon and weather data would be from 2 hours ago)</p>
+            <p className="dissappear-medium">(For example, 2 would mean balloon and weather data would be from 2 hours ago)</p>
             <input type="number" onChange={updateVal} value={val} max={23} min={0}/>
         </div>
     );
